731cd56befbbbb998d1c7b3ccfdacdae44cfb9ff
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-07-07 14:30:33 +0200

    INFRA: Initialize Web App

---
+++ OnDotnetLive/Program.cs
@@ -0,0 +1,32 @@
+// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. 
+//  W/ love for all .NET developers from around the world.
+// ---------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using OnDotnetLive.Applications;
+
+namespace OnDotnetLive
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<MainApplication>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}

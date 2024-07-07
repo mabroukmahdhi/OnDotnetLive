6fbe9a115ffbd49f35e4fb6cf746320c357661b1
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-07-07 15:24:31 +0200

    INFRA: Initialize Test Project

---
+++ OnDotnetLive.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. 
+//  W/ love for all .NET developers from around the world.
+// ---------------------------------------------------------------
+
+namespace OnDotnetLive.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}

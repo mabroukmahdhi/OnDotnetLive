4e5bde3588914b9d3771a018900b3be8b4ae58a1
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-07-07 18:04:36 +0200

    COMPONENTS: TalkComponent

---
+++ OnDotnetLive/Views/Bases/CardBase.razor.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. 
+//  W/ love for all .NET developers from around the world.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace OnDotnetLive.Views.Bases
+{
+    public partial class CardBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string Height { get; set; } = "100%";
+
+        [Parameter]
+        public string Width { get; set; } = "100%";
+    }
+}

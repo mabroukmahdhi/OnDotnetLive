02723ae6ed687754d66a760f04eb886b25832f33
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-07-07 16:38:26 +0200

    BASES: NavMenu

---
+++ OnDotnetLive/Views/Bases/NavMenuBase.razor.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. 
+//  W/ love for all .NET developers from around the world.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace OnDotnetLive.Views.Bases
+{
+    public partial class NavMenuBase : ComponentBase
+    {
+        private bool expanded = true;
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
